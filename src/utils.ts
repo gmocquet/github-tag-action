3dd4fc1b3411530dc9b5788e79f8a591de5e81cb
Guillaume MOCQUET <1082289+gmocquet@users.noreply.github.com>
2022-05-09 16:02:21 +0200

    feat: add option to getLatestTag to match prefixRegex

--- src/utils.ts
+++ src/utils.ts
@@ -59,7 +59,11 @@ export function getLatestTag(
   tagPrefix: string
 ) {
   return (
-    tags.find((tag) => !prerelease(tag.name.replace(prefixRegex, ''))) || {
+    tags.find(
+      (tag) =>
+        !prerelease(tag.name.replace(prefixRegex, '')) &&
+        tag.name.match(prefixRegex)
+    ) || {
       name: `${tagPrefix}0.0.0`,
       commit: {
         sha: 'HEAD',
